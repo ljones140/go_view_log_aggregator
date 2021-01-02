8dc9c28f7bba3b65af67f3f59c2e02d386873696
Lewis Jones <lewis.jones@smartpension.co.uk>
2021-01-02 18:12:57 +0000

    Add incrementation, simple code that passes test

--- page.go
+++ page.go
@@ -1,6 +1,12 @@
 package main
 
+import "net"
+
 type Page struct {
 	Name   string
 	Visits int
+}
+
+func (p *Page) AddVisit(address net.IP) {
+	p.Visits = p.Visits + 1
 }
--- page_test.go
+++ page_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"net"
+	"testing"
+)
 
 func TestPageCreation(t *testing.T) {
 	t.Run("it is created with a page name", func(t *testing.T) {
@@ -13,6 +16,17 @@ func TestPageCreation(t *testing.T) {
 		page := Page{Name: "index"}
 		if page.Visits != 0 {
 			t.Errorf("number of vists got %d wanted %d", page.Visits, 0)
+		}
+	})
+}
+
+func TestAddingPageVisits(t *testing.T) {
+	t.Run("visit count is incremented when visit is added", func(t *testing.T) {
+		page := Page{Name: "index"}
+		address := net.ParseIP("192.0.2.1")
+		page.AddVisit(address)
+		if page.Visits != 1 {
+			t.Errorf("number of visits got %d wanted %d", page.Visits, 1)
 		}
 	})
 }
